80455d50dfabf1a453e3a4c7c1db5159139c7666
karammm <56406583+karammm@users.noreply.github.com>
2023-03-11 22:22:57 +0530

    html file using form-group

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,41 +1,22 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  name = 'Angular 5';
-  gender="male";
-  about="";
-  submitted=false;
-  user={
-    username:'',
-    email:'',
-    gender:'',
-    about:''
-  }
-  @ViewChild('f') signupForm: NgForm;
-  onFormSubmit(f: NgForm) {
-    this.submitted=true;
-    this.user.username= this.signupForm.value.userData.username;
-    this.user.email= this.signupForm.value.userData.email;
-    this.user.gender= this.signupForm.value.userData.gender;
-    this.user.about= this.signupForm.value.userData.about;
-    this.signupForm.reset();
-  }
-
-  fillValues(){
-    //for seting all values use setValue and to set few values use patchValue
-    this.signupForm.form.setValue({
-      userData:{
-        email:'karam@siila.org',
-        username:"karam"
-      },
-      gender:'male',
-      about:'About US'
+export class AppComponent implements OnInit {
+  genders=['male','female'];
+  signUpForm:FormGroup;
+  ngOnInit():void{
+    this.signUpForm= new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('female')
     })
+  }
+  onSubmit(){
+    console.log(this.signUpForm)
   }
 }
