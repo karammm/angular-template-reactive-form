b557d4ec7860e9537bd0c6718224acf24809aad0
karammm <56406583+karammm@users.noreply.github.com>
2023-03-11 20:35:43 +0530

    1 file modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,20 +10,32 @@ export class AppComponent {
   name = 'Angular 5';
   gender="male";
   about="";
+  submitted=false;
+  user={
+    username:'',
+    email:'',
+    gender:'',
+    about:''
+  }
   @ViewChild('f') signupForm: NgForm;
   onFormSubmit(f: NgForm) {
-    console.log(this.signupForm);
+    this.submitted=true;
+    this.user.username= this.signupForm.value.userData.username;
+    this.user.email= this.signupForm.value.userData.email;
+    this.user.gender= this.signupForm.value.userData.gender;
+    this.user.about= this.signupForm.value.userData.about;
+    this.signupForm.reset();
   }
 
   fillValues(){
     //for seting all values use setValue and to set few values use patchValue
-    this.signupForm.form.patchValue({
+    this.signupForm.form.setValue({
       userData:{
         email:'karam@siila.org',
         username:"karam"
       },
-      // gender:'male',
-      // about:'About US'
+      gender:'male',
+      about:'About US'
     })
   }
 }
