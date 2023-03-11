256adb6596f262d04d59fe1937d9d532a1896c3b
karammm <56406583+karammm@users.noreply.github.com>
2023-03-12 00:52:17 +0530

    Grouping the Controls in the Reactive Forms

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,8 +11,10 @@ export class AppComponent implements OnInit {
   signUpForm:FormGroup;
   ngOnInit():void{
     this.signUpForm= new FormGroup({
-      'username': new FormControl(null,Validators.required),
-      'email': new FormControl(null,[Validators.required,Validators.email]),
+      'userData': new FormGroup({
+        'username': new FormControl(null,Validators.required),
+        'email': new FormControl(null,[Validators.required,Validators.email]),
+      }),
       'gender': new FormControl('female',Validators.required)
     })
   }
