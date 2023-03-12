976906d42094c40388119de348d93d6e06d56e27
karammm <56406583+karammm@users.noreply.github.com>
2023-03-12 11:11:19 +0530

    Dynamically Add Form Controls with FormArray

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
@@ -9,16 +9,25 @@ import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders=['male','female'];
   signUpForm:FormGroup;
+  get hobbyControls(){
+    return (<FormArray>this.signUpForm.get('hobbies')).controls;
+  }
   ngOnInit():void{
     this.signUpForm= new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null,Validators.required),
         'email': new FormControl(null,[Validators.required,Validators.email]),
       }),
-      'gender': new FormControl('female',Validators.required)
+      'gender': new FormControl('female',Validators.required),
+      'hobbies': new FormArray([])//list of form controls
     })
   }
   onSubmit(){
     console.log(this.signUpForm)
+  }
+
+  onAddHobby(){
+    const control = new FormControl(null,[Validators.required]);
+    (<FormArray>this.signUpForm.get('hobbies')).push(control);
   }
 }
